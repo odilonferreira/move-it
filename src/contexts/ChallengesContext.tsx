e64f73fe5eae8a562237caf2eb6c22968b069b04
Odilon Ferreira <odilon.aferreira@gmail.com>
2021-02-27 20:04:31 -0300

    creating context to share data to all components

---
+++ src/contexts/ChallengesContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, ReactNode, useState } from "react";
+
+interface ChallengeContextData {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+  levelUp: () => void;
+  startNewChallenge: () => void;
+}
+
+interface ChallengesProviderProps {
+  children: ReactNode;
+}
+
+export const ChallengesContext = createContext({} as ChallengeContextData);
+
+export function ChallengesProvider({ children }: ChallengesProviderProps) {
+  const [level, setLevel] = useState(1);
+  const [currentExperience, setCurrentExperience] = useState(0);
+  const [challengesCompleted, setChallengesCompleted] = useState(0);
+
+  function levelUp() {
+    setLevel(level + 1);
+  }
+
+  function startNewChallenge() {}
+
+  return (
+    <ChallengesContext.Provider
+      value={{
+        level,
+        currentExperience,
+        challengesCompleted,
+        levelUp,
+        startNewChallenge,
+      }}
+    >
+      {children}
+    </ChallengesContext.Provider>
+  );
+}
